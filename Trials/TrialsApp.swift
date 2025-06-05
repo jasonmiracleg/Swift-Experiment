53345cd4fe6fd98219322f28af0d3a97a10390c5
jasonmiracleg <jasonmiracle15@gmail.com>
2025-06-05 08:53:02 +0700

    Initial Commit

---
+++ Trials/TrialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialsApp.swift
+//  Trials
+//
+//  Created by Jason Miracle Gunawan on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TrialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrialsTests/TrialsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrialsTests.swift
+//  TrialsTests
+//
+//  Created by Jason Miracle Gunawan on 05/06/25.
+//
+
+import Testing
+@testable import Trials
+
+struct TrialsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
